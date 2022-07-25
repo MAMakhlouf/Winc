3f9e93a425003048c43eaa9df0f01f99f3b15238
MAMakhlouf <107413891+MAMakhlouf@users.noreply.github.com>
2022-07-25 15:28:49 +0200

    f

---
+++ color-toggle_assignment/script/script.js
@@ -0,0 +1,31 @@
+//Checks the status of the nav menu opened or closed?
+let toggleNavStatus = false;
+
+let toggleNav = function () {
+  let getSidebar = document.querySelector(".nav-sidebar");
+  let getSidebarUl = document.querySelector(".nav-sidebar ul");
+  let getSidebarTitle = document.querySelector(".nav-sidebar span");
+  let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+  if (toggleNavStatus === false) {
+    getSidebarUl.style.visibility = "visible";
+    getSidebar.style.width = "452px";
+    getSidebarTitle.style.opacity = "0.5";
+
+    let arrayLenght = getSidebarLinks.length;
+    for (let i = 0; i < arrayLenght.lenght; i++) {
+      getSidebarLinks[i].style.opacity = "1";
+    }
+    toggleNavStatus = true;
+  } else if (toggleNavStatus === true) {
+    getSidebar.style.width = "50px";
+    getSidebarTitle.style.opacity = "0";
+
+    let arrayLenght = getSidebarLinks.length;
+    for (let i = 0; i < arrayLenght.lenght; i++) {
+      getSidebarLinks[i].style.opacity = "0";
+    }
+    getSidebarUl.style.visibility = "visible";
+    toggleNavStatus = false;
+  }
+};
